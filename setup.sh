a7f2427850b18160282b4db34843743992c91663
SeungWoo3 <rohseungwoo@gmail.com>
2024-09-25 15:57:59 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+wget https://raw.githubusercontent.com/jongchank/wsl-config-files/main/inputrc -O ~/.inputrc
+wget https://raw.githubusercontent.com/jongchank/wsl-config-files/main/vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/jongchank/wsl-config-files/main/hello.c
+gcc hello.c
+./a.out
+# git clone https://github.com/jongchank/autose
+
